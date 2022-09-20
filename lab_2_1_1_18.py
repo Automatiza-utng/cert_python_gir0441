bb2af973baa31309d7243d36300e880fdbdacd2a
gbarron2014 <gbarron@utng.edu.mx>
2022-09-20 09:45:11 -0500

    Labs 18 y 19

---
+++ lab_2_1_1_18.py
@@ -0,0 +1,8 @@
+'''
+Descripciòn Uso de las funciones Sep y End
+Autor: Gabriel Barròn
+Fecha: 20 Sep 2022
+'''
+print("Fundamentos","Programación","en", sep="***", end="...")
+print("Python")
+
---
+++ lab_2_1_1_19.py
@@ -0,0 +1,39 @@
+'''
+Descripciòn: Hacer la flecha al doble y duplicarla de lado a lado
+Autor: Gabriel Barròn
+Fecha: 20 Sep 2022
+'''
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print('Flecha al doble')
+print("         *")
+print("       *   *")
+print("      *     *")
+print("     *       *")
+print("    *         *")
+print("   *           *")
+print("  *             *")
+print(" *               *")
+print("*****         *****")
+print("    *         *")
+print("    *         *")
+print("    *         *")
+print("    *         *")
+print("    *         *")
+print("    *         *")
+print("    **********")
+
+print('Duplicar flecha') 
+
+
+
+
+
+
